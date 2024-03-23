29c17b05fba72db86f577291915fdb49387761ea
Alyx Olivier <alyx.olivier@gmail.com>
2024-03-23 15:16:53 -0400

    Refactoring

--- analyse_laue.py
+++ analyse_laue.py
@@ -4,143 +4,122 @@
 https://github.com/juham58/PHY-3003---Diffraction-rayons-X
 """
 
-from pathlib import Path
-import re
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import re
 
 plt.rcParams.update({'font.size': 16})
 
-def round_to_half(number):
-    return round(number*2)/2
+cristaux = {"NaCl": 564.02, "LiF": 402.80, "Si": 543.10}
 
-def check_if_half(number):
-    if number.is_integer():
-        return False
-    else:
-        return True
+def laue_import(distance_cristal: float, fichier: bytes, cristal: str) -> pd.DataFrame:
+    L = distance_cristal
+    a = cristaux[cristal]
 
-def laue_import(distance_cristal, filename, a):
-    R = distance_cristal
-    data = pd.read_csv(filename)
+    data = pd.read_csv(os.fsdecode(fichier))
     data.set_index(" ", inplace=True)
     data.drop(["Area", "Mean", "Min", "Max"], axis=1, inplace=True)
-    data["X"] = (data["X"]-data.loc[1, "X"])/1e6
-    data["Y"] = (data["Y"]-data.loc[1, "Y"])/1e6
-    data["r"] = np.sqrt(data["X"]**2+data["Y"]**2)
 
-    data["u"] = -1/(np.tan(0.5*np.arctan(data["r"]/R)))*(data["X"]/data["r"])
-    data["v"] = -1/(np.tan(0.5*np.arctan(data["r"]/R)))*(data["Y"]/data["r"])
+    data["X"] = (data["X"] - data.loc[1, "X"]) / 1e6
+    data["Y"] = (data["Y"] - data.loc[1, "Y"]) / 1e6
+    data["Z"] = (np.sqrt(data["X"]**2 + data["Y"]**2 + L**2) - L + 1e-308)
 
-    data["h"] = np.nan
-    data["k"] = np.nan
-    data["l"] = np.nan
+    data["u"] = data["X"] / data["Z"]
+    data["v"] = data["Y"] / data["Z"]
 
-    for index, u in enumerate(data["u"]):
-        if index != 0:
-            u_rounded = round_to_half(u)
-            data.loc[index+1, "u_rounded"] = u_rounded
-            if check_if_half(u_rounded):
-                l = 2
-            else:
-                l = 1
-            h = u_rounded*l
-            data.loc[index+1, "h"] = h
-            data.loc[index+1, "l"] = l
-        else:
-            data.loc[index+1, "h"] = 0
-            data.loc[index+1, "l"] = 0
+    data["h"] = np.rint(data["u"]).astype("int64")
+    data["k"] = np.rint(data["v"]).astype("int64")
+    data["l"] = 1
 
-        for index, v in enumerate(data["v"]):
-            if index != 0:
-                v_rounded = round_to_half(v)
-                data.loc[index+1, "v_rounded"] = v_rounded
-                if check_if_half(v_rounded):
-                    l = 2
-                else:
-                    l = 1
-                k = v_rounded*l
-                data.loc[index+1, "k"] = k
-            else:
-                data.loc[index+1, "k"] = 0
+    data[["h", "k", "l"]] = data[["h", "k", "l"]].where((data["h"] % 2 == 1) & (data["k"] % 2 == 1), 2*data[["h", "k", "l"]])
 
     
-    d_hkl = a/np.sqrt(data["h"]**2+data["k"]**2+data["l"]**2)
-    n = data["h"]**2+data["k"]**2+data["l"]**2
-    #data["d_hkl"] = d_hkl
-    data["n"] = n
-    data["lambda_exp"] = 2*d_hkl*np.sin(0.5*np.arctan(data["r"]/R))
-    data["lambda_the"] = 2*d_hkl*data["l"]/np.sqrt(data["h"]**2+data["k"]**2+data["l"]**2)
-    data["lambda_error"] = np.abs((data["lambda_exp"]-data["lambda_the"])/data["lambda_the"])*100
-    print(filename, n)
+    data["n"] = data["h"]**2 + data["k"]**2 + data["l"]**2
+    data["d_hkl"] = a / np.sqrt(data["h"]**2 + data["k"]**2 + data["l"]**2)
+
+    data["lambda_exp"] = 2 * data["d_hkl"] * np.sin(0.5 * np.arctan(np.sqrt(data["X"]**2 + data["Y"]**2) / L))
+    data["lambda_the"] = 2 * data["d_hkl"] * np.sin(np.arctan (data["l"] / np.sqrt (data["h"]**2 + data["k"]**2)))
+    data["lambda_error"] = np.abs((data["lambda_exp"] - data["lambda_the"]) / data["lambda_the"]) * 100
 
     return data
 
-def laue_mass_import(crystal):
-    if crystal == "si":
-        dir = Path.cwd()/"si"
-        a = 542e-12
-    if crystal == "nacl":
-        dir = Path.cwd()/"nacl"
-        a = 562e-12
-    if crystal == "lif":
-        dir = Path.cwd()/"lif"
-        a = 402e-12
-    files = dir.glob("*.csv")
-    for result in files:
-        distance = float(str(result).split("_")[1][0:2])*1e-3
-        results_path = str(result).split("\\")[-1]+"_results.csv"
-        laue_import(distance, result, a).to_csv(Path.cwd()/"Résultats"/results_path)
+def laue_mass_import(cristal: str):
+    data_dir = os.fsencode("Data/" + cristal)
+    resultats_dir = os.fsencode("Resultats/" + cristal)
+
+    for fichier in os.listdir(data_dir):
+        if os.fsdecode(fichier).endswith(".csv"):
+            data = os.path.join(data_dir, fichier)
+            resultats = os.path.join(resultats_dir, fichier)
+
+            distance_str = os.fsdecode(fichier).split("_")[1]
+            distance = float(re.findall("\d+", distance_str)[0]) * 1e-3
+
+            laue_import(distance, data, cristal).to_csv(os.fsdecode(resultats))
 
 
-def laue_graph():
-    dir = Path.cwd()/"Résultats"
-    files = dir.glob("*.csv")
-    mean_dataframe = pd.DataFrame(columns=["Nom", "Moyenne", "Écart type"])
+def laue_graph(cristal: str):
+    resultats_dir = os.fsencode("Resultats/" + cristal)
+    images_dir = os.path.join(resultats_dir, os.fsencode("Images"))
+
+    mean_dataframe = pd.DataFrame()
     index = 0
-    for result in files:
-        index += 1
-        data = pd.read_csv(result)
-        mean_values = pd.DataFrame({"Nom": str(result).split("\\")[-1], "Moyenne": data["lambda_error"].mean(), 
-        "Écart type": data["lambda_error"].std()}, index = [index])
-        mean_dataframe = mean_dataframe.append(mean_values, ignore_index=True)
-        crystal_name = str(result).split("\\")[-1].split("_")[0]
-        if crystal_name == "nacl":
-            crystal_name = "NaCl"
-        if crystal_name == "lif":
-            crystal_name = "LiF"
-        if  crystal_name == "si":
-            crystal_name = "Si"
-        distance_value = str(result).split("\\")[-1].split("_")[1][0:2]
-        voltage_at_peak = str(result).split("\\")[-1].split("_")[2][0:2]
-        number_of_images = str(result).split("\\")[-1].split("_")[3][0:2]
-        table_title = "Données acquises avec le cristal de {}, à une distance de {} mm, \nune tension au pic de {} kV et un nombre d'images moyennées de {}.".format(crystal_name, distance_value, voltage_at_peak, number_of_images)
 
-        # Imprime les tableaux brutes en LaTeX
-        #print(data.to_latex(columns=["X", "Y", "u", "v", "h", "k", "l", "n", "lambda_exp", "lambda_the", "lambda_error"], caption=table_title, label="tab:"+str(result).split("\\")[-1], column_format="|l|r|r|r|r|r|r|r|r|r|r|r|"))
-        
-        plt.figure(figsize=(7,7))
-        plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
-        plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
-        plt.scatter(data["u"], data["v"])
-        plt.xlabel(r"$u=h/l$")
-        plt.ylabel(r"$v=k/l$")
-        plt.grid()
-        plt.savefig(str(result)+".png")
+    for fichier in os.listdir(resultats_dir):
+        if os.fsdecode(fichier).endswith(".csv"):
 
-    # Imprime le tableau de l'annexe B
-    #print(mean_dataframe.to_latex(label="tab:tableau_erreurs_moyennes", column_format="|l|r|r|r|"))
+            resultats = os.path.join(resultats_dir, fichier)
+            image_name = os.fsencode(os.fsdecode(fichier).removesuffix(".csv") + ".pdf")
+            image = os.path.join(images_dir, image_name)
 
-# Importe les données en format DataFrame
-laue_mass_import("lif")
-laue_mass_import("nacl")
-laue_mass_import("si")
+            index += 1
+            data = pd.read_csv(os.fsdecode(resultats))
+            mean_values = pd.DataFrame({"Nom": os.fsdecode(fichier), "Moyenne": data["lambda_error"].mean(), 
+                                        "Écart type": data["lambda_error"].std()}, index = [index])
+            mean_dataframe = pd.concat([mean_dataframe, mean_values], ignore_index=True)
 
-# Affiche les graphiques et les tableaux
-laue_graph()
+            distance_str = os.fsdecode(fichier).split("_")[1]
+            distance = re.findall("\d+", distance_str)[0]
+
+            voltage_str = os.fsdecode(fichier).split("_")[2]
+            voltage = re.findall("\d+", voltage_str)[0]
+
+            nb_images_str = os.fsdecode(fichier).split("_")[3]
+            nb_images = re.findall("\d+", nb_images_str)[0]
+
+            table_title = f"Données acquises avec le cristal de {cristal}, à une distance de {distance} mm, \nune tension au pic de {voltage} kV et un nombre d'images moyennées de {nb_images}."
+
+            # Imprime les données brutes en LaTeX
+            #print(data.to_latex(columns=["X", "Y", "Z", "u", "v", "h", "k", "l", "n", "d_hkl", "lambda_exp", "lambda_the", "lambda_error"], caption=table_title, label="tab:" + os.fsdecode(fichier) column_format="ccccccccccccc"))
+            
+            plt.figure(figsize=(7,7))
+            plt.style.use("ggplot")
+            plt.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
+            plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
+            plt.scatter(data["u"], data["v"])
+            plt.xlabel(r"$u=h/l$")
+            plt.ylabel(r"$v=k/l$")
+            plt.title(table_title)
+            plt.savefig(os.fsdecode(image))
+
+            # Imprime le tableau de l'annexe B
+            #print(mean_dataframe.to_latex(label="tab:tableau_erreurs_moyennes", column_format="|l|r|r|r|"))
 
 
-plt.show()
+def main():
+    # Importe les données en format DataFrame
+    laue_mass_import("LiF")
+    laue_mass_import("NaCl")
+    laue_mass_import("Si")
 
+    # Affiche les graphiques et les tableaux
+    laue_graph("NaCl")
+
+    #plt.show()
+
+
+if __name__ == "__main__":
+    main()
